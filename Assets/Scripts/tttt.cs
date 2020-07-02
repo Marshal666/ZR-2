0d4f4784ffbf753055bb3384a0f5006b64ae00b5
Fi3ldMarshal <Fi3ldMarshal@users.noreply.github.com>
2020-07-02 19:40:34 +0200

    performed tests

--- Assets/Scripts/tttt.cs
+++ Assets/Scripts/tttt.cs
@@ -5,12 +5,77 @@ using System;
 using System.Text;
 using System.Linq;
 using System.Diagnostics;
+using JetBrains.Annotations;
 
 public class tttt : MonoBehaviour
 {
+
+
+    #region MARRAYNAV
+    /*
+    MArray<int> arr;
+    MArray<GameObject> arro;
+    int[] pos;
+    */
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
+
+
+        #region MARRAYTEST
+        /*
+        MArray<int> arr = new MArray<int>(4, 5, 3);
+        for(int i = 0; i < arr.OneDimensional.Length; i++)
+        {
+            arr.OneDimensional[i] = i;
+        }
+
+        for(int i = 0; i < arr.Dimensions[2]; i++)
+        {
+            for(int j = 0; j < arr.Dimensions[1]; j++)
+            {
+                for (int k = 0; k < arr.Dimensions[0]; k++) {
+                    print(string.Format("({0}, {1}, {2}) ", k, j, i) + arr[k, j, i]);
+                }
+            }
+        }
+
+        bool res = true;
+        int ci = 0;
+
+        for (int i = 0; i < arr.Dimensions[2]; i++)
+        {
+            for (int j = 0; j < arr.Dimensions[1]; j++)
+            {
+                for (int k = 0; k < arr.Dimensions[0]; k++)
+                {
+                    res &= arr[k, j, i] == arr.OneDimensional[ci];
+                    ci++;
+                }
+            }
+        }
+        print(res);
+        */
+
+        #endregion
+
+        #region MARRAYNAV
+        /*
+        arr = new MArray<int>(5, 4);
+        arro = new MArray<GameObject>(5, 4);
+        pos = new int[arr.Dimensions.Length];
+        for(int i = 0; i < arr.OneDimensional.Length; i++)
+        {
+            arr.OneDimensional[i] = i;
+            arro[i] = new GameObject("obj" + i);
+            int[] crds = arro.getCoords(i);
+            arro[i].transform.position = new Vector3(crds[0], crds[1]) * 2;
+            arro[i].AddComponent<TextMesh>().text = i.ToString();
+        }
+        */
+        #endregion
 
         #region 4DTEST
         /*
@@ -79,6 +144,86 @@ public class tttt : MonoBehaviour
         }
 
         print(s);*/
+        #endregion
+
+        #region FIZZBUZZ
+        /*
+        StringBuilder sb = new StringBuilder();
+
+        int[] nums = { 3, 5, 7 };
+        string[] words = { "fizz", "buzz", "duzz" };
+
+        for(int i = 1; i <= 105; i++)
+        {
+            bool set = false;
+            for(int j = 0; j < nums.Length; j++)
+            {
+                if(i % nums[j] == 0)
+                {
+                    set = true;
+                    sb.Append(words[j]);
+                }
+            }
+            if(!set)
+            {
+                sb.Append(i);
+            }
+
+            sb.Append("\n");
+        }
+        print(sb.ToString());
+        */
+        #endregion
+
+        #region CELLDATAT
+        /*CellData dt = (0, 0, 0, 0);
+        print(dt.ToString());*/
+        #endregion
+
+        #region ROADGEN
+
+        GameObject g = new GameObject();
+        g.AddComponent<MeshFilter>().mesh = RoadGenerator.GenerateRoadGrid(1, 1);
+        g.AddComponent<MeshRenderer>();
+
+        #endregion
+    }
+
+    private void Update()
+    {
+
+        #region MARRAYNAV
+        /*
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Visit(0, -1);
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            Visit(0, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Visit(1, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Visit(1, -1);
+        }
+
+        void Visit(int dim, int d)
+        {
+            int dt = pos[dim] + d;
+            if (dt >= 0 && dt < arr.Dimensions[dim])
+            {
+                pos[dim] += d;
+
+                //FIX: MArray.getIndex doesn't work well
+                arr[pos]--;
+                arro[pos].GetComponent<TextMesh>().text = arr[pos].ToString();
+                print("Move: " + string.Join(", ", pos) + " i: " + arr.getIndex(pos));
+            }
+        }*/
         #endregion
 
     }
