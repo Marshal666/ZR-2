708b2886426cbcca3518c0a2619de6821be2cf7c
Fi3ldMarshal <Fi3ldMarshal@users.noreply.github.com>
2020-07-07 22:11:13 +0200

    added Player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Reposition()
+    {
+        World w = World.main;
+        if (w.Cells != null && w.currentPosition.Length > 0)
+        {
+            Vector3 newPlayerPos = new Vector3(w.currentPosition[0] * w.buildingDistance, w.Cells[w.currentPosition].Data.Number1);
+            if (w.currentPosition.Length > 1)
+            {
+                newPlayerPos.z = w.currentPosition[1] * w.buildingDistance;
+            }
+            transform.position = newPlayerPos;
+        }
+    }
+}
