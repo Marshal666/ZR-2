03b55114bad8dd3342a58ee7263ed3173890c6c8
Fi3ldMarshal <Fi3ldMarshal@users.noreply.github.com>
2020-07-07 22:10:06 +0200

    added Scene class for basic interactions in scene

---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene : MonoBehaviour
+{
+
+    Transform rootTransform;
+
+    public GameObject root;
+
+    public static Scene main;
+
+    public static GameObject Root { get { return main.root; } }
+
+    public static Transform RootTransform { get { return main.rootTransform; } }
+
+    public GameObject player;
+
+    Player playerC;
+
+    public static GameObject PlayerObject { get { return main.player; } }
+
+    public static Player Player { get { return main.playerC; } }
+
+    private void Awake()
+    {
+        main = this;
+
+        if (!root)
+        {
+            root = GameObject.Find("Root");
+            if(!root)
+            {
+                root = new GameObject("Root");
+            }
+        }
+
+        rootTransform = root.transform;
+
+        if(!player)
+        {
+            player = GameObject.Find("Player");
+            if(!player)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("Player does not exist");
+#endif
+            }
+        }
+
+        playerC = player.GetComponent<Player>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+
+    public static void ClearRoot()
+    {
+        if(RootTransform)
+        {
+            for(int i = RootTransform.childCount - 1; i >= 0; i--)
+            {
+                Destroy(RootTransform.GetChild(i).gameObject);
+            }
+        }
+    }
+
+}
