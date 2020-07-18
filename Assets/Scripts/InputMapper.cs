1ea2ab28ec39aca20dd46aef6dba2f593a66a4d6
Marshal666 <Marshal666@users.noreply.github.com>
2020-07-18 12:24:10 +0200

    added InputMapper

---
+++ Assets/Scripts/InputMapper.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DefaultExecutionOrder(-1)]
+public class InputMapper : MonoBehaviour
+{
+
+    [System.Serializable]
+    public struct DimensionInputs
+    {
+        public KeyCode positive;
+        public KeyCode negative;
+    }
+
+    public DimensionInputs[] dimensionInputs;
+
+    public KeyCode UndoKey = KeyCode.B, RedoKey = KeyCode.N;
+
+    public static InputMapper main;
+
+    public int[] moveDimension;
+
+    public int[] MoveDimension {  get { return main.moveDimension; } }
+
+    public bool Undo;
+
+    public bool Redo;
+
+    private void Awake()
+    {
+        main = this;
+
+        moveDimension = new int[dimensionInputs != null ? dimensionInputs.Length : 0];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < dimensionInputs.Length; i++)
+        {
+            if (Input.GetKeyDown(dimensionInputs[i].positive))
+            {
+                moveDimension[i] = 1;
+            }
+            else if (Input.GetKeyDown(dimensionInputs[i].negative))
+            {
+                moveDimension[i] = -1;
+            } else
+            {
+                moveDimension[i] = 0;
+            }
+
+            Undo = Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(UndoKey);
+
+            Redo = Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Y) || Input.GetKeyDown(RedoKey);
+
+        }
+    }
+}
