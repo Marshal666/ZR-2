cfe92cf9a5c06e8781d0af0578e0c5ccfc2fb89f
Marshal666 <Marshal666@users.noreply.github.com>
2020-07-18 12:24:31 +0200

    added IGameEvent interface for game events

---
+++ Assets/Scripts/IGameEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameEventExecutionResult
+{
+    Failed,
+    Success,
+}
+
+public interface IGameEvent
+{
+
+    void Execute();
+
+    void Revert();
+
+    GameEventExecutionResult result { get; }
+
+}
