c8c5b661fcb0781ea3a3356515da65a4cca1ea86
Marshal666 <Marshal666@users.noreply.github.com>
2020-07-20 13:33:59 +0200

    now checks for win/lose condition

--- Assets/Scripts/Scene.cs
+++ Assets/Scripts/Scene.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class Scene : MonoBehaviour
 {
 
+    public enum GameStates
+    {
+        Playing,
+        Paused,
+        Menu,
+        Editor,
+        //more required?
+    }
+
+    public StateMachine<GameStates> GameState;
+
     Transform rootTransform;
 
     public GameObject root;
@@ -29,8 +40,22 @@ public class Scene : MonoBehaviour
 
     private void Awake()
     {
+
         main = this;
 
+        //playing is temporary starting state
+        GameState = new StateMachine<GameStates>(GameStates.Playing);
+
+        //game state methods assignment
+        GameState.Methods[GameStates.Playing] = Playing;
+        GameState.Methods[GameStates.Paused] = Paused;
+        GameState.Methods[GameStates.Menu] = Menu;
+        GameState.Methods[GameStates.Editor] = Editor;
+
+        GameState.Switches[GameStates.Playing][GameStates.Paused] = Pause;
+        GameState.Switches[GameStates.Paused][GameStates.Playing] = Unpause;
+
+        //find or create root if needed
         if (!root)
         {
             root = GameObject.Find("Root");
@@ -40,8 +65,10 @@ public class Scene : MonoBehaviour
             }
         }
 
+        //assign root transform
         rootTransform = root.transform;
 
+        //search for player if it hasn't been assigned
         if(!player)
         {
             player = GameObject.Find("Player");
@@ -53,11 +80,70 @@ public class Scene : MonoBehaviour
             }
         }
 
+        //find players component
         playerC = player.GetComponent<Player>();
 
+        //create event system object
         es = new EventSystem();
 
     }
+
+    #region STATE_METHODS
+
+    void Playing()
+    {
+
+        //for optimization sake, don't do this every frame? But when player makes a move!
+        if (CheckPlayerWin())
+        {
+            PlayerWin();
+            return;
+        }
+
+        if (CheckPlayerLose())
+        {
+            PlayerLose();
+            return;
+        }
+
+        if (InputMapper.main.Undo)
+        {
+            es.Undo();
+        }
+
+        if (InputMapper.main.Redo)
+        {
+            es.Redo();
+        }
+
+    }
+
+    void Paused()
+    {
+
+    }
+
+    void Menu()
+    {
+
+    }
+
+    void Editor()
+    {
+
+    }
+
+    void Pause()
+    {
+
+    }
+
+    void Unpause()
+    {
+
+    }
+
+    #endregion
 
     // Start is called before the first frame update
     void Start()
@@ -68,15 +154,7 @@ public class Scene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(InputMapper.main.Undo)
-        {
-            es.Undo();
-        }
-
-        if(InputMapper.main.Redo)
-        {
-            es.Redo();
-        }
+        GameState.Execute();
     }
 
     public static void ClearRoot()
@@ -88,6 +166,51 @@ public class Scene : MonoBehaviour
                 Destroy(RootTransform.GetChild(i).gameObject);
             }
         }
+    }
+
+    public static void PlayerWin()
+    {
+        print("player won");
+        main.playerC.PlayerState.State = Player.PlayerStates.Won;
+        main.GameState.State = GameStates.Paused;
+    }
+
+    public static void PlayerLose()
+    {
+        print("player lost");
+        main.playerC.PlayerState.State = Player.PlayerStates.Lost;
+        main.GameState.State = GameStates.Paused;
+    }
+
+    public static bool CheckPlayerWin()
+    {
+        
+        if(main.GameState.State == GameStates.Playing)
+        {
+            switch (World.main.GameType)
+            {
+                case World.GameTypes.SumToZero:
+                    if(World.main.Sum == 0)
+                    {
+                        return true;
+                    }
+                    break;
+                case World.GameTypes.ReachPoints:
+                    break;
+                default:
+                    break;
+            }
+        }
+        return false;
+    }
+
+    public static bool CheckPlayerLose()
+    {
+        if (main.GameState.State == GameStates.Playing)
+        {
+            //TODO: ??? as player cannot currently die because of EventSystem to drive it back
+        }
+        return false;
     }
 
 }
