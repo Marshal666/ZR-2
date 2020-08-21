531f19c0ee07040b8909ede2fef3ae593e8f25b6
Marshal666 <Marshal666@users.noreply.github.com>
2020-08-22 01:17:31 +0200

    added more data/references for game to work

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -18,21 +18,61 @@ public class GameData : MonoBehaviour
     /// Indexes of blocks for cell drawing
     /// </summary>
     public List<GameObject> buildingBlocks = new List<GameObject>();
-
-    /// <summary>
-    /// Indexes of blocks for cell drawing
-    /// </summary>
     public static List<GameObject> BuildingBlocks { get { return main.buildingBlocks; } }
 
     /// <summary>
     /// Material used for roads
     /// </summary>
     public Material roadMaterial;
+    public static Material RoadMaterial {  get { return main.roadMaterial; } }
 
     /// <summary>
-    /// Material used for roads
+    /// UI object that stores levels access buttons
     /// </summary>
-    public static Material RoadMaterial {  get { return main.roadMaterial; } }
+    public RectTransform levelsContentHolder;
+    public static RectTransform LevelsContentHolder { get { return main.levelsContentHolder; } }
+
+    /// <summary>
+    /// Folder with level files
+    /// </summary>
+    public string localLevelsDirectory;
+    public static string LocalLevelsDirectory { get { return main.localLevelsDirectory; } }
+
+    /// <summary>
+    /// LoadLevel button prefab
+    /// </summary>
+    public GameObject loadLevelButton;
+    public static GameObject LoadLevelButton { get { return main.loadLevelButton; } }
+
+    /// <summary>
+    /// Time scale when game is paused (0)
+    /// </summary>
+    public float pausedTimeScale = 0f;
+    public static float PausedTimeScale { get { return main.pausedTimeScale; } }
+
+    /// <summary>
+    /// Normal game speed
+    /// </summary>
+    public float unPausedTimeScale = 1f;
+    public static float UnPausedTimeScale { get { return main.unPausedTimeScale; } }
+
+    /// <summary>
+    /// Default cell transparency - not transparent
+    /// </summary>
+    public float defaultCellAlphaColor = 1f;
+    public static float DefaultCellAlphaColor { get { return main.defaultCellAlphaColor; } }
+
+    /// <summary>
+    /// Cell transparency used in views - half transparent
+    /// </summary>
+    public float semiTransparentCellColor = 0.5f;
+    public static float SemiTransparentCellColor { get { return main.semiTransparentCellColor; } }
+
+    /// <summary>
+    /// 3D Arrow for pointing where teleport teleports to
+    /// </summary>
+    public GameObject arrowObject;
+    public static GameObject ArrowObject { get { return main.arrowObject; } }
 
     /// <summary>
     /// Script init
