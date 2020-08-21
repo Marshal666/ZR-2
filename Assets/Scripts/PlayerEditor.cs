736bbb6c59f7a453c71ad7e474699d286099001d
Marshal666 <Marshal666@users.noreply.github.com>
2020-08-22 01:15:56 +0200

    Player script component should now show PlayerState while playing

---
+++ Assets/Scripts/PlayerEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Player))]
+[CanEditMultipleObjects]
+public class PlayerEditor : Editor
+{
+
+    SerializedProperty currentPos;
+
+    private void OnEnable()
+    {
+        currentPos = serializedObject.FindProperty("CurrentPosition");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(currentPos);
+        Player pl = (Player)target;
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("State: ");
+        if (pl && pl.PlayerState != null)
+        {
+            Player.PlayerStates st = (Player.PlayerStates)EditorGUILayout.EnumPopup(pl.PlayerState.State);
+            if (st != pl.PlayerState.State)
+            {
+                pl.PlayerState.SwitchState(st);
+            }
+        }
+        EditorGUILayout.EndHorizontal();
+
+    }
+
+}
