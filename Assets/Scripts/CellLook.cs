6ccb4b0a213d14d136cc6138f2633becbda049ae
Marshal666 <Marshal666@users.noreply.github.com>
2020-09-09 20:15:39 +0200

    Cell look definition

--- Assets/Scripts/CellLook.cs
+++ Assets/Scripts/CellLook.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Defines how cell should look in different states
+/// Values assigned in inspector in GameData class
+/// </summary>
 [System.Serializable]
 public class CellLook
 {
