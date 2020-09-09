96b445d66a055abda273a71ff79aaaf4607e6255
Marshal666 <Marshal666@users.noreply.github.com>
2020-09-09 20:13:35 +0200

    Definition of what WorldRenderer should be able to do

---
+++ Assets/Scripts/IWorldRenderer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IWorldRenderer
+{
+
+    void RenderPositionChanges();
+
+    float BuildingDistance { get; set; }
+
+}
